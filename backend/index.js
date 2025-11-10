e2f9d52b649ea8891cf26c939d91a6821873d737
Safvan K.p <safvankp730@gmail.com>
2025-11-10 18:33:51 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -32,8 +32,9 @@ app.get('/api/scan/:id/pdf', downloadPdf);
 app.get('/api/saved-reports',getSaveReports)
 app.post('/api/saved-reports',saveReport)
 app.delete('/api/saved-reports/:id', deleteSavedReport); 
-app.get('/ping', (req, res) => {
-  res.status(200).send("ok");
+app.get("/ping", (req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  res.send("ok");
 });
 
 
